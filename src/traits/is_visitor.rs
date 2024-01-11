a1426aa3701e507e04ff226bed061cd4473bc671
Danilo Guanabara <danilo@sensorial.systems>
2024-01-11 19:23:19 -0300

    Simplified VisitorConstructor

--- src/traits/is_visitor.rs
+++ src/traits/is_visitor.rs
@@ -2,14 +2,16 @@ use crate::{KnowsVisitor, KnowsParent, KnowsValue};
 
 pub trait IsVisitor<'a>: Sized {
     fn visit<Child: KnowsVisitor<'a>>(&'a self, value: Child) -> Child::Visitor
-    where Child::Visitor: VisitorConstructor<'a, Owned = Child::Visitor, Value = Child>,
+    where Child::Visitor: VisitorConstructor<'a, Value = Child>,
           &'a Self: Into<<Child::Visitor as KnowsParent<'a>>::Parent>
     {
         Child::Visitor::new_with_parent(self.into(), value)
     }
 }
 
-pub trait VisitorConstructor<'a>: KnowsParent<'a> + KnowsValue<'a> {
-    type Owned: KnowsParent<'a> + KnowsValue<'a>;
-    fn new_with_parent(parent: <Self::Owned as KnowsParent<'a>>::Parent, value: <Self::Owned as KnowsValue<'a>>::Value) -> Self::Owned;
+pub trait VisitorConstructor<'a>: KnowsParent<'a> + KnowsValue<'a>
+where Self::Value: KnowsVisitor<'a>,
+      <Self::Value as KnowsVisitor<'a>>::Visitor: KnowsParent<'a>,
+{
+    fn new_with_parent(parent: <<Self::Value as KnowsVisitor<'a>>::Visitor as KnowsParent<'a>>::Parent, value: Self::Value) -> <Self::Value as KnowsVisitor<'a>>::Visitor;
 }
--- src/visitor/is_visitor.rs
+++ src/visitor/is_visitor.rs
@@ -1,4 +1,4 @@
-use crate::{IsVisitor, Visitor, KnowsParent, KnowsValue, HasValue, VisitorConstructor};
+use crate::{IsVisitor, Visitor, KnowsParent, KnowsValue, HasValue, VisitorConstructor, KnowsVisitor};
 
 impl<'a, Parent, Value> KnowsParent<'a> for Visitor<Parent, Value> {
     type Parent = Parent;
@@ -30,19 +30,20 @@ where Value: Clone
 
 impl<'a, Parent, Value> IsVisitor<'a> for Visitor<Parent, Value> {}
 
-impl<'a, Parent, Value> VisitorConstructor<'a> for Visitor<Parent, Value> {
-    type Owned = Visitor<Parent, Value>;
-    fn new_with_parent(parent: Parent, value: Value) -> Self::Owned {
+impl<'a, Parent, Value> VisitorConstructor<'a> for Visitor<Parent, Value>
+where Value: KnowsVisitor<'a, Visitor = Visitor<Parent, Value>>
+{
+    fn new_with_parent(parent: Parent, value: Value) -> Visitor<Parent, Value> {
         Visitor::new_with_parent(parent, value)
     }
 }
 
 impl<'a, Parent, Value> IsVisitor<'a> for &'a Visitor<Parent, Value> {}
 
-impl<'a, Parent, Value> VisitorConstructor<'a> for &'a Visitor<Parent, Value> {
-    type Owned = Visitor<Parent, Value>;
-    fn new_with_parent(_parent: Parent, _value: Value) -> Self::Owned {
-        todo!()
-        // Visitor::new_with_parent(parent, value)
+impl<'a, Parent, Value> VisitorConstructor<'a> for &'a Visitor<Parent, Value>
+where Value: KnowsVisitor<'a, Visitor = Visitor<Parent, Value>>,
+{
+    fn new_with_parent(parent: Parent, value: Value) -> Visitor<Parent, Value> {
+        Visitor::new_with_parent(parent, value)
     }
 }
