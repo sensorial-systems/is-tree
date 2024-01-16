3946af8d34c9de9a985fbccc03f0c9e743cd5daa
Danilo Guanabara <danilo@sensorial.systems>
2024-01-16 11:58:19 -0300

    Removing unused HasPathGet

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -3,7 +3,6 @@
 pub mod has_parent;
 pub mod has_root;
 pub mod has_path_segment;
-pub mod has_path_get;
 pub mod has_visitor;
 pub mod has_get;
 pub mod has_relative_access;
@@ -23,7 +22,6 @@ pub use has_parent::*;
 pub use has_root::*;
 pub use has_path_segment::*;
 pub use has_get::*;
-// pub use has_path_get::*;
 pub use has_visitor::*;
 pub use has_relative_access::*;
 
