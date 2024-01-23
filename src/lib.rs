8a60ce862917d1e85c9775e8a89e1fbf87ccc1cb
Danilo Guanabara <danilo@sensorial.systems>
2024-01-23 00:55:17 -0300

    Start of iterator rework

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 pub mod traits;
 pub mod path;
-// pub mod iterator;
+pub mod iterator;
 pub mod visitor;
 
 pub use traits::*;
 pub use path::*;
-// pub use iterator::*;
+pub use iterator::*;
 pub use visitor::*;
 
 pub use is_tree_macro::*;
