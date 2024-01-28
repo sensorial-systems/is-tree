7358f54bd8771c662cc25eeeb3cec4dffa7648e8
Danilo Guanabara <danilo@sensorial.systems>
2024-01-28 15:13:08 -0300

    Generalizing HasBranches constraints for &Visitor

--- src/visitor/has_branches.rs
+++ src/visitor/has_branches.rs
@@ -1,23 +1,24 @@
 use crate::*;
 
 impl<'a, Parent, Value> KnowsBranches<'a> for Visitor<Parent, Value>
-where &'a Value: KnowsBranches<'a> + 'a,
-      <&'a Value as KnowsBranches<'a>>::Branches: KnowsVisitor<'a>
+where Value: KnowsBranches<'a>,
+      Value::Branches: KnowsVisitor<'a>
 {
-    type Branches = <<&'a Value as KnowsBranches<'a>>::Branches as KnowsVisitor<'a>>::Visitor;
+    type Branches = <<Value as KnowsBranches<'a>>::Branches as KnowsVisitor<'a>>::Visitor;
 }
 
 impl<'a, Parent, Value> HasBranches<'a> for &'a Visitor<Parent, Value>
 where Parent: Clone,
-      &'a Value: Clone + HasBranches<'a>,
-      <&'a Value as KnowsBranches<'a>>::Branches: KnowsPathSegment + KnowsVisitor<'a>,
-      <<&'a Value as KnowsBranches<'a>>::Branches as KnowsVisitor<'a>>::Visitor: KnowsPathSegment<PathSegment = <<&'a Value as KnowsBranches<'a>>::Branches as KnowsPathSegment>::PathSegment>,
-      Self::Branches: HasVisitorConstructor<'a, Value = <&'a Value as KnowsBranches<'a>>::Branches>,
+      Value: Clone + HasBranches<'a>,
+      Value::Branches: KnowsPathSegment + KnowsVisitor<'a>,
+      <Value::Branches as KnowsVisitor<'a>>::Visitor: KnowsPathSegment<PathSegment = <Value::Branches as KnowsPathSegment>::PathSegment>,
+      Self::Branches: HasVisitorConstructor<'a, Value = Value::Branches>,
       Visitor<Parent, Value>: Into<<Self::Branches as KnowsParent<'a>>::Parent> + Clone,
 {
     fn branches(self) -> impl Iterator<Item = Self::Branches> {
         self
             .value // TODO: How to use .value() here instead?
+            .clone() 
             .branches()
             .map(|value| self.visit(value))
     }
