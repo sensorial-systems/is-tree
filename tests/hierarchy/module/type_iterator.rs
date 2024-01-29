8fe4f2a522b6ce73578fe1cde66cd35a80365cf0
Danilo Guanabara <danilo@sensorial.systems>
2024-01-29 02:24:19 -0300

    Fixing iter_type parents

--- tests/hierarchy/module/type_iterator.rs
+++ tests/hierarchy/module/type_iterator.rs
@@ -15,9 +15,7 @@ impl<'a> TypeIter<'a, Visitor<Visitors<'a, &'a Library, &'a Module>, &'a String>
         let parent = parent.unwrap();
         let visitor: Visitor<Visitors<'_, &Library, &Module>, &String> = Visitor::new(parent.clone(), &self.name);
         collection.push(visitor.clone());
-        // FIXME: The parent seems incorrect.
-        // Correct one should be:
-        // let parent: Visitors<'_, &Library, &Module> = visitor.into();
+        let parent: Visitors<'_, &Library, &Module> = Visitor::new(parent.clone().into(), self).into();
         collection.extend(self.children.iter().flat_map(|child| child.iter_type_with_parent::<&String>(Some(parent.clone()))));
         collection.into()
     }
@@ -27,9 +25,12 @@ impl<'a> TypeIter<'a, Visitor<Visitors<'a, &'a Library, &'a Module>, &'a mut Str
     fn type_iterator(self, parent: Option<Visitors<'a, &'a Library, &'a Module>>) -> TypeIterator<Visitor<Visitors<'a, &'a Library, &'a Module>, &'a mut String>> {
         let mut collection = Vec::new();
         let parent = parent.unwrap();
+        // FIXME: This is a workaround. We can wrap this in a safe function.
+        let self_ = unsafe { &mut *(self as *mut Module) };
         let visitor = Visitor::new(parent.clone(), &mut self.name);
         collection.push(visitor);
-        // FIXME: The parent seems incorrect.
+        let parent: Visitor<ModuleParentVisitor<'_>, &Module> = Visitor::new(parent.clone().into(), self_).into();
+        let parent: Visitors<'_, &Library, &Module> = parent.into();
         collection.extend(self.children.iter_mut().flat_map(|child| child.iter_type_with_parent::<&mut String>(Some(parent.clone()))));
         collection.into()
     }
