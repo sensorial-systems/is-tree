06fcd1ed5d0a49f4349ab7bb707d56ab1b9cb144
Danilo Guanabara <danilo@sensorial.systems>
2024-01-30 13:21:02 -0300

    Adding TODO notes

--- tests/hierarchy/library/has_get.rs
+++ tests/hierarchy/library/has_get.rs
@@ -2,4 +2,5 @@ use is_tree::HasGet;
 
 use super::Library;
 
+// TODO: derive HasGet
 impl<'a> HasGet<'a> for &'a Library {}
--- tests/hierarchy/module/has_get.rs
+++ tests/hierarchy/module/has_get.rs
@@ -2,4 +2,5 @@ use is_tree::HasGet;
 
 use super::Module;
 
+// TODO: derive HasGet
 impl<'a> HasGet<'a> for &'a Module {}
