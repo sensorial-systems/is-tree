cec259aa626bb30aca1a7c94067078dae6788b92
Danilo Guanabara <danilo@sensorial.systems>
2024-04-02 01:25:59 -0300

    Cleaning up code

--- is-tree/tests/hierarchy/module/type_iterator.rs
+++ is-tree/tests/hierarchy/module/type_iterator.rs
@@ -11,28 +11,26 @@ impl<'a> KnowsVisitorOf<'a, String> for &'a mut Module {
 
 impl<'a> TypeIter<'a, Visitor<Visitors<'a, &'a Library, &'a Module>, &'a String>> for &'a Module {
     fn type_iterator(self, parent: Option<Visitors<'a, &'a Library, &'a Module>>) -> TypeIterator<Visitor<Visitors<'a, &'a Library, &'a Module>, &'a String>> {
-        let mut collection = Vec::new();
         let parent = parent.unwrap();
-        let visitor: Visitor<Visitors<'_, &Library, &Module>, &String> = Visitor::new(parent.clone(), &self.name);
-        collection.push(visitor.clone());
+        let mut visitors = vec![Visitor::new(parent.clone(), &self.name)];
         let parent: Visitors<'_, &Library, &Module> = Visitor::new(parent.clone().into(), self).into();
-        collection.extend(self.children.iter().flat_map(|child| child.iter_type_with_parent::<String>(Some(parent.clone()))));
-        collection.into()
+        visitors.extend(self.children.iter().flat_map(|child| child.iter_type_with_parent::<String>(Some(parent.clone()))));
+        visitors.into()
     }
 }
 
 impl<'a> TypeIter<'a, Visitor<Visitors<'a, &'a Library, &'a Module>, &'a mut String>> for &'a mut Module {
     fn type_iterator(self, parent: Option<Visitors<'a, &'a Library, &'a Module>>) -> TypeIterator<Visitor<Visitors<'a, &'a Library, &'a Module>, &'a mut String>> {
-        let mut collection = Vec::new();
+        let mut visitors = Vec::new();
         let parent = parent.unwrap();
         // FIXME: This is a workaround. We can wrap this in a safe function.
         let self_ = unsafe { &mut *(self as *mut Module) };
         let visitor = Visitor::new(parent.clone(), &mut self.name);
-        collection.push(visitor);
+        visitors.push(visitor);
         let parent: Visitor<ModuleParentVisitor<'_>, &Module> = Visitor::new(parent.clone().into(), self_).into();
         let parent: Visitors<'_, &Library, &Module> = parent.into();
-        collection.extend(self.children.iter_mut().flat_map(|child| child.iter_type_with_parent::<String>(Some(parent.clone()))));
-        collection.into()
+        visitors.extend(self.children.iter_mut().flat_map(|child| child.iter_type_with_parent::<String>(Some(parent.clone()))));
+        visitors.into()
     }
 }
 
