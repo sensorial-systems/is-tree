7a1073dc01548d0869fa0143c54423bbf2f82ca3
Danilo Guanabara <danilo@sensorial.systems>
2024-06-03 16:51:05 -0300

    Cleaning up codebase

--- is-tree-macro/src/type_/enumeration/traits/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod has_parent;
-pub mod has_root;
-pub mod has_path_segment;
-pub mod has_branches;
-pub mod has_get;
-pub mod has_relative_access;
--- is-tree-macro/src/type_/mod.rs
+++ is-tree-macro/src/type_/mod.rs
@@ -1,22 +1,16 @@
 use crate::traits::Derive;
 
-mod enumeration;
 mod structure;
-mod path;
 
-pub use enumeration::*;
 pub use structure::*;
-pub use path::*;
 
 pub enum Type {
-    Enumeration(Enumeration),
     Structure(Structure)
 }
 
 impl Derive for Type {
     fn derive(&mut self) -> proc_macro2::TokenStream {
         match self {
-            Type::Enumeration(enumeration) => enumeration.derive(),
             Type::Structure(structure) => structure.derive(),
         }
     }
@@ -25,9 +19,8 @@ impl Derive for Type {
 impl From<syn::DeriveInput> for Type {
     fn from(ast: syn::DeriveInput) -> Self {
         match ast.data.clone() {
-            syn::Data::Enum(data) => Type::Enumeration(Enumeration::from((ast, data))),
             syn::Data::Struct(data) => Type::Structure(Structure::from((ast, data))),
-            syn::Data::Union(_) => panic!("IsTree cannot be derived for unions"),
+            _ => panic!("IsTree cannot be derived for unions and enumerations"),
         }
     }
 }
