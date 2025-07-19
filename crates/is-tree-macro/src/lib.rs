219024b10912d9b169515330d0fef7e789ca2b17
Danilo Guanabara <danilo@magicblock.gg>
2025-07-19 01:29:07 -0300

    Reorganizing workspace

--- crates/is-tree-macro/src/lib.rs
+++ crates/is-tree-macro/src/lib.rs
@@ -8,6 +8,9 @@ use type_::Type;
 
 #[proc_macro_derive(IsTree, attributes(tree))]
 pub fn is_tree(input: TokenStream) -> TokenStream {
+    std::panic::set_hook(Box::new(|info| {
+        eprintln!("proc-macro panic: {}", info);
+    }));
     let ast = syn::parse_macro_input!(input as syn::DeriveInput);
     Type::from(ast).derive().into()
 }
